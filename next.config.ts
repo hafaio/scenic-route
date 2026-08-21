22d157ba5b36a183c6f7a154a10d8385e8eb562c
Erik Brinkman <erik.brinkman@gmail.com>
2026-08-20 22:08:19 -0400

    fix: deploy
    needed to be .js

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-export default {
-  images: { unoptimized: true },
-  output: "export",
-  // basePath is injected at deploy time by actions/configure-pages (static_site_generator: next), not hardcoded here
-} satisfies NextConfig;
